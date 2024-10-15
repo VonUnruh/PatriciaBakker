fe0dcedbbb0e5a3b9aee4f39182db777cc34067b
VonUnruh <54571125+VonUnruh@users.noreply.github.com>
2024-10-15 22:21:11 +0200

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -78,8 +78,8 @@ let tlStory1 = gsap.timeline();
 tlStory1.from("#story1", { yPercent: 100, opacity: 0 })
         .from("#story2", { yPercent: 100, opacity: 0 })
         .from("#story3", { yPercent: 100, opacity: 0 })
-        .fromTo("#story4", { opacity: 0 }, { yPercent: -5, opacity: 0.7 })
-	.to("#storyBlock1", { yPercent: 100 });
+        .fromTo("#story4", { opacity: 0 }, { yPercent: -15, opacity: 0.7 })
+	.to("#storyBlock1", { yPercent: 10 });
 
 ScrollTrigger.create({
   animation: tlStory1,
